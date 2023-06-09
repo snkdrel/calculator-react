d8d397f31be7f5120f1c725ccab459828bf60d7e
snkdrel <ces.zop@gmail.com>
2023-06-09 14:04:39 -0600

    Change type of firstOperant to string

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,35 +3,43 @@ import Display from "./Display";
 import { useState } from 'react';
 
 function Calculator() {
-    const [firstOperand, setFirstOperand] = useState(0);
+    const [firstOperand, setFirstOperand] = useState('0');
 
     // functions to alter operands
     function appendDigit(clickedValue) {
-        const newOperand = firstOperand.toString().concat(clickedValue);
-        setFirstOperand( Number(newOperand) );
+        if(firstOperand === '0') {
+            setFirstOperand( clickedValue );
+        } else {
+            setFirstOperand( firstOperand.concat(clickedValue) );
+        }
     }
 
     function switchSign() {
-        setFirstOperand( firstOperand * -1 );
+        if( firstOperand !== '0') {
+            if(firstOperand.charAt() === '-') {
+                setFirstOperand( firstOperand.slice(1) );
+            } else {
+                setFirstOperand( firstOperand.padStart(firstOperand.length + 1, '-') );
+            }
+        }
     }
 
     function resetToZero() {
-        setFirstOperand(0);
+        setFirstOperand('0');
     }
 
     function backspace() {
-        const newOperand = firstOperand.toString();
-        
-        if(newOperand.length === 1 || (newOperand.length === 2 && newOperand.charAt() === '-') ) {
-            setFirstOperand(0);
+        if(firstOperand.length === 1 || (firstOperand.length === 2 && firstOperand.charAt() === '-') ) {
+            resetToZero(0);
         } else {
-            setFirstOperand( Number(newOperand.slice(0, newOperand.length-1)) );
+            setFirstOperand( firstOperand.slice(0, firstOperand.length-1) );
         }
     }
 
     function addDecimalPoint() {
-        const newOperand = firstOperand.toString().concat('.');
-        setFirstOperand( Number(newOperand) );
+        if( firstOperand.indexOf('.') === -1 ) {
+            setFirstOperand( firstOperand.concat('.') );   
+        }
     }
 
     return(
@@ -45,16 +53,16 @@ function Calculator() {
                 <Button text={'/'}/>
                 <Button text={'='}/>
 
-                <Button text={'7'} onClickButton={() => appendDigit(7)}/>
-                <Button text={'8'} onClickButton={() => appendDigit(8)}/>
-                <Button text={'9'} onClickButton={() => appendDigit(9)}/>
-                <Button text={'4'} onClickButton={() => appendDigit(4)}/>
-                <Button text={'5'} onClickButton={() => appendDigit(5)}/>
-                <Button text={'6'} onClickButton={() => appendDigit(6)}/>
-                <Button text={'1'} onClickButton={() => appendDigit(1)}/>
-                <Button text={'2'} onClickButton={() => appendDigit(2)}/>
-                <Button text={'3'} onClickButton={() => appendDigit(3)}/>
-                <Button text={'0'} onClickButton={() => appendDigit(0)}/>
+                <Button text={'7'} onClickButton={() => appendDigit('7')}/>
+                <Button text={'8'} onClickButton={() => appendDigit('8')}/>
+                <Button text={'9'} onClickButton={() => appendDigit('9')}/>
+                <Button text={'4'} onClickButton={() => appendDigit('4')}/>
+                <Button text={'5'} onClickButton={() => appendDigit('5')}/>
+                <Button text={'6'} onClickButton={() => appendDigit('6')}/>
+                <Button text={'1'} onClickButton={() => appendDigit('1')}/>
+                <Button text={'2'} onClickButton={() => appendDigit('2')}/>
+                <Button text={'3'} onClickButton={() => appendDigit('3')}/>
+                <Button text={'0'} onClickButton={() => appendDigit('0')}/>
 
                 <Button text={'.'} onClickButton={addDecimalPoint}/>
 
